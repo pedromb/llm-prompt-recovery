afc9bfb631c16408eca8229118bd57e62ca3df47
Pedro Bernardo <pedromagalhaesbernardo@gmail.com>
2024-04-06 21:19:04 +0000

    update code

--- src/train/lora_mistral_7b_unsloth.py
+++ src/train/lora_mistral_7b_unsloth.py
@@ -8,11 +8,11 @@ from trl import DataCollatorForCompletionOnlyLM, SFTTrainer
 from src.utils import json_parser_from_chat_response, DATA_PATH
 from unsloth import FastLanguageModel
 
-DATASET =  str(DATA_PATH / "new_data_for_training.csv" )
-PROJECT = "mistral-7b-lora-finetuned-unsloth"
+DATASET =  str(DATA_PATH / "final_data.csv" )
+PROJECT = "finetuned-unsloth-new"
 BASE_MODEL_NAME = "mistral-7b-instruct-v0.2"
 RUN_NAME = BASE_MODEL_NAME + "-" + PROJECT
-RESUME_FROM_CHECKPOINT = "/home/llm-prompt-recovery/data/weights/mistral-7b-instruct-v0.2-mistral-7b-lora-finetuned-unsloth/checkpoint-675"
+RESUME_FROM_CHECKPOINT = None
 OUTPUT_DIR = str(DATA_PATH  / "weights" / RUN_NAME)
 
 def create_dataset_item(df_row):
@@ -29,7 +29,7 @@ def create_dataset_item(df_row):
 def load_dataset(df, tokenizer, max_size = None):
     if max_size is not None and max_size < len(df):
         df = df.sample(n=max_size)
-    df = df.sample(frac=1)
+    df = df.sample(frac=1, random_state = 1)
     all_data = [create_dataset_item(row) for _, row in df.iterrows()]
     final_data = []
     for entry in tqdm(all_data, desc="Filtering data..."):
@@ -48,22 +48,13 @@ def format_single(input, output):
             'rewritten_text': 'A version of original_text that was rewritten by an LLM according to a specific prompt.'
         }}
 
-        Given the task of understanding how text is rewritten by analyzing the original_text and rewritten_text, your goal is to deduce the specific instructions or prompt that was most likely used to generate the rewritten text from the original text. Consider the changes made in terms of style, tone, structure, and content. Assess whether the rewrite focuses on summarization, paraphrasing, stylistic alteration (e.g., formal to informal), or any specific content changes (e.g., making the text more concise, expanding on ideas, or altering the perspective). Follow this steps:
-
-        1. Read the original_text: Start by thoroughly understanding the content, style, tone, and purpose of the original text. Note any key themes, technical terms, and the overall message.
-        2. Analyze the rewritten_text: Examine how the rewritten text compares to the original. Identify what has been changed, added, or omitted. Pay close attention to changes in style (formal, informal), tone (serious, humorous), structure (paragraph order, sentence structure), and any shifts in perspective or emphasis.
-        3. Infer the Prompt: Based on your analysis, infer the most likely prompt that guided the rewriting process. Your inference should account for the observed changes in style, tone, structure, and content. Specify the type of task (e.g., summarize, paraphrase, make more accessible to a general audience), any specific directions evident from the changes, and any specific stylistic choice.
-
-        Based on your analysis return the prompt as if you were given the instruction your self like:
-        "Rewrite this text..."
-        "Transform this ... into ... based on the style of ..."
+        Your job is to return the prompt that was used to transform the original_text into the rewritten_text.
         
         Make the prompt short and direct using a maximum of 20 words.
 
         Return your answer using the following JSON structure:
         {{"prompt": "Your best guess for the prompt used"}}
     
-            
         Return a valid JSON as output and nothing more.
         
         -----------------------
@@ -104,7 +95,7 @@ model, tokenizer = FastLanguageModel.from_pretrained(
 )
 
 tokenizer.pad_token_id =  tokenizer.unk_token_id
-tokenizer.padding_side = 'right'
+tokenizer.padding_side = 'left'
 
 
 model = FastLanguageModel.get_peft_model(
@@ -126,10 +117,7 @@ data = pd.read_csv(DATASET)
 cluster_to_split = json.load(open("/home/llm-prompt-recovery/data/cluster_to_split.json"))
 data["split"] = data.cluster.apply(lambda x: cluster_to_split.get(str(x), "train"))
 train = data.loc[data.split == "train"]
-val = data.loc[data.split == "val"].sample(200)
-
 train_dataset = load_dataset(train, tokenizer)
-val_dataset = load_dataset(val, tokenizer)
 response_template = "[/INST]"
 data_collator = DataCollatorForCompletionOnlyLM(response_template, tokenizer=tokenizer)
 
@@ -138,21 +126,20 @@ trainer = SFTTrainer(
     tokenizer=tokenizer,
     formatting_func=formatting_func,
     train_dataset=train_dataset,
-    eval_dataset=val_dataset,
     max_seq_length=2048,
     args=transformers.TrainingArguments(
         output_dir=OUTPUT_DIR,
         warmup_steps=0,
-        num_train_epochs=3,
+        num_train_epochs=1,
         per_device_train_batch_size=16,
         learning_rate=5e-5, # Want a small lr for finetuning
         optim="paged_adamw_8bit",
         lr_scheduler_type="cosine",
-        logging_steps=25,
+        logging_steps=20,
         logging_dir="./logs",
         bf16=True,
         save_strategy="steps",
-        save_steps=25,
+        save_steps=20,
         evaluation_strategy="no",
         save_total_limit=5,
     ),
--- src/train/merge_model_unsloth.py
+++ src/train/merge_model_unsloth.py
@@ -1,9 +1,12 @@
+"""
+Merge the finetuned lora adapters with the main model and save the weights
+"""
 import torch
 from unsloth import FastLanguageModel
 DEVICE = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 
-PEFT_MODEL = "/home/llm-prompt-recovery/data/weights/mistral-7b-instruct-v0.2-mistral-7b-lora-finetuned-unsloth/checkpoint-675"
-OUTPUT_PATH = "/home/llm-prompt-recovery/data/weights/mistral-7b-finetuned-lora-unsloth"
+PEFT_MODEL = "/home/llm-prompt-recovery/data/weights/mistral-7b-instruct-v0.2-finetuned-subject-prompt/checkpoint-1250"
+OUTPUT_PATH = "/home/llm-prompt-recovery/data/weights/mistral-7b-finetuned-subject-prompt"
 
 
 model, tokenizer = FastLanguageModel.from_pretrained(
